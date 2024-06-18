42403c38c30b33dd600db573ef63a12fb8a15223
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-06-17 22:02:02 -0300

    feat: create signup form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,26 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    ],
+  
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit{
+  loginOrSignup: boolean = false
   formLogin!: FormGroup
+  formSignup!: FormGroup
 
   constructor(private formBuilder: FormBuilder){}
 
   ngOnInit(){
     this.formLogin = this.formBuilder.group({
-      email: '',
-      password: ''
+      l_email: '',
+      l_password: ''
+    })
+    this.formSignup = this.formBuilder.group({
+      s_email: '',
+      s_password: '',
+      s_password_confirm: ''
     })
   }
 
   onSubmit(){
     console.log("foi")
+  }
+
+  changeForms(){
+    this.loginOrSignup = !this.loginOrSignup
   }
 }

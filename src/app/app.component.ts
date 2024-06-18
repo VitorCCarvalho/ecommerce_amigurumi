1092030bdb87b19aa19b5c802b295016726790f0
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-06-18 11:53:17 -0300

    fix: changed app to display main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./pages/login/login.component";
+import { MainComponent } from "./pages/main/main.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet, LoginComponent]
+    imports: [CommonModule, RouterOutlet, MainComponent]
 })
 export class AppComponent {
   title = 'ecommerce_amigurumi';

276bd037433e6560813a01e873dac0bedb918d20
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-06-19 15:06:46 -0300

    feat: add Cherry bomb one font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from "./pages/main/main.component";
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet, MainComponent]
+    imports: [CommonModule, RouterOutlet, MainComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'ecommerce_amigurumi';
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { FeaturedListComponent } from "../../components/featured-list/featured-list.component";
 
 @Component({
     selector: 'app-main',
     standalone: true,
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, FeaturedListComponent]
 })
 export class MainComponent {
 

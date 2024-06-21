c83259f2020cf6e1e349d4610f38046a648fd647
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-06-21 20:04:17 -0300

    feat: style changes

--- src/app/components/featured-item/featured-item.component.ts
+++ src/app/components/featured-item/featured-item.component.ts
@@ -1,41 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FeaturedItem } from '../../types/featured-item.type';
-import { animate, group, query, style, transition, trigger } from '@angular/animations';
-
-const left = [
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
-  group([
-    query(':enter', [style({ transform: 'translateX(-100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
-      optional: true,
-    }),
-    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(100%)' }))], {
-      optional: true,
-    }),
-  ]),
-];
-
-const right = [
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
-  group([
-    query(':enter', [style({ transform: 'translateX(100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
-      optional: true,
-    }),
-    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(-100%)' }))], {
-      optional: true,
-    }),
-  ]),
-];
 
 @Component({
   selector: 'app-featured-item',
   standalone: true,
   imports: [],
-  animations: [
-    trigger('animSlider', [
-      transition(':increment', right),
-      transition(':decrement', left)
-    ])
-  ],
   templateUrl: './featured-item.component.html',
   styleUrl: './featured-item.component.scss'
 })

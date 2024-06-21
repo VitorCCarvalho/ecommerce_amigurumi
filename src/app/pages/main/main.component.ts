f3ee25b1375e649efc60da1d8923bfee99d37221
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-06-21 20:04:55 -0300

    fix: added Footer and ProductList import

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FeaturedListComponent } from "../../components/featured-list/featured-list.component";
+import { ProductListComponent } from "../../components/product-list/product-list.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
     selector: 'app-main',
     standalone: true,
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss',
-    imports: [HeaderComponent, FeaturedListComponent]
+    imports: [HeaderComponent, FeaturedListComponent, ProductListComponent, FooterComponent]
 })
 export class MainComponent {
 

11ebf5d0d4493416a12c8439c5322ec6476ae6c8
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-06-21 19:57:29 -0300

    feat: create shopping-item

---
+++ src/app/components/shopping-item/shopping-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../types/product.type';
+
+@Component({
+  selector: 'app-shopping-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './shopping-item.component.html',
+  styleUrl: './shopping-item.component.scss'
+})
+export class ShoppingItemComponent {
+  @Input() product: Product ={
+    id: 0,
+    name: 'Bear',
+    price: 39.99,
+    imgSrc: "/assets/png/bear.png"
+  }
+}

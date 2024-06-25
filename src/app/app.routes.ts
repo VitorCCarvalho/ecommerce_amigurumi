35d20481d81cb96a70e97f62c185abeb60d6e2df
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-06-25 10:11:01 -0300

    feat: create item page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ItemPageComponent } from './pages/item-page/item-page.component';
 
 export const routes: Routes = [
     {path: '', component: MainComponent},
     { path: 'cart', component: ShoppingCartComponent},
-    { path: 'account', component: LoginComponent}
+    { path: 'account', component: LoginComponent},
+    { path: 'item', component: ItemPageComponent}
 ];

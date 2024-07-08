796b736f4943e4e0b47f3869d179a947b270147d
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-07-08 12:47:33 -0300

    feat: added fadein and fadeout anim to grid item

--- src/app/components/grid-item/grid-item.component.ts
+++ src/app/components/grid-item/grid-item.component.ts
@@ -1,11 +1,35 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../types/product.type';
 import { Router } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-grid-item',
   standalone: true,
   imports: [],
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        transition(
+          ':enter', 
+          [
+            style({ opacity: 0 }),
+            animate('0.7s linear', 
+                    style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave', 
+          [
+            style({ opacity: 1 }),
+            animate('0.7s linear', 
+                    style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ],
   templateUrl: './grid-item.component.html',
   styleUrl: './grid-item.component.scss'
 })

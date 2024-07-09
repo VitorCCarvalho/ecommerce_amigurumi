47d770dd630b462df3e5e14103d9d64e503e27a1
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2024-07-09 12:07:38 -0300

    fix: fixed featuredList increment and decrement anim

--- src/app/components/featured-list/featured-list.component.ts
+++ src/app/components/featured-list/featured-list.component.ts
@@ -3,30 +3,8 @@ import { FeaturedItemComponent } from "../featured-item/featured-item.component"
 import { FeaturedItem } from '../../types/featured-item.type';
 import { CommonModule } from '@angular/common';
 import { animate, group, query, style, transition, trigger } from '@angular/animations';
+import { Router } from '@angular/router';
 
-const left = [
-    query(':enter, :leave', style({ position: 'fixed'}), { optional: true }),
-    group([
-      query(':enter', [style({ transform: 'translateX(-100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
-        optional: true,
-      }),
-      query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(100%)' }))], {
-        optional: true,
-      }),
-    ]),
-  ];
-  
-  const right = [
-    query(':enter, :leave', style({ position: 'fixed' }), { optional: true }),
-    group([
-      query(':enter', [style({ transform: 'translateX(100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
-        optional: true,
-      }),
-      query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(-100%)' }))], {
-        optional: true,
-      }),
-    ]),
-  ];
 
 @Component({
     selector: 'app-featured-list',
@@ -34,10 +12,27 @@ const left = [
     templateUrl: './featured-list.component.html',
     styleUrl: './featured-list.component.scss',
     animations: [
-        trigger('animSlider', [
-            transition(':increment', right),
-            transition(':decrement', left),
-          ]),
+          trigger(
+            'inOutAnimation', 
+            [
+              transition(
+                ':enter', 
+                [
+                  style({ opacity: 0}),
+                  animate('0.5s ease-in', 
+                          style({ opacity: 1 }))
+                ]
+              ),
+              transition(
+                ':leave', 
+                [
+                  style({ opacity: 1 }),
+                  animate('0.5s ease-out', 
+                          style({ opacity: 0 }))
+                ]
+              )
+            ]
+          )
     ],
     imports: [
         FeaturedItemComponent,
@@ -47,16 +42,20 @@ export class FeaturedListComponent {
     counter: number = 0;
     featuredItems: FeaturedItem[] = [
         {
-            text: 'Amigurumis por apenas R$49,99',
-            imgSrc: "/assets/png/bear.png"
+            text: 'Amigurumis por apenas R$24,99',
+            imgSrc: "/assets/png/seal.png",
+            link: 'all-items'
         },
         {
             text: 'Aproveite as nossas promoções',
-            imgSrc: "/assets/png/dragon.png"
+            imgSrc: "/assets/png/dragon.png",
+            link: 'all-items'
         },
         {
             text: 'Novo amigurumi de gato a venda',
-            imgSrc: "/assets/png/cat.png"
+            imgSrc: "/assets/png/cat.png",
+            link: 'item',
+            queryParams: 'Cat'
         }
     ]
 
